083e358c6036e25ea7abbf2ca052122d0d7c611f
yzn <66845701@qq.com>
2018-06-30 20:23:49 +0800

    czv

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,6 @@
-import * as Rx from 'rxjs/observable'
-
-
-console.log(Rx);
+import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
+ 
+range(1, 200)
+  .pipe(filter(x => x % 2 === 1), map(x => x + x))
+  .subscribe(x => console.log(x));
