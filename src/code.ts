d15c3495668152566d58ece3cd10f9b4b14b0f0f
yzn <66845701@qq.com>
2018-06-30 20:33:57 +0800

    daf

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,16 @@
 import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
 import { map, filter, switchMap } from 'rxjs/operators';
  
-range(1, 200)
-  .pipe(filter(x => x % 2 === 1), map(x => x + x))
-  .subscribe(x => console.log(x));
+var obs=Observable.create(function  subscribe(observer:any){
+    observer.next('Hey guys')
+});
+
+obs.subscribe( (x:any)=>addItem(x));
+
+
+function addItem(val:any){
+    var node=document.createElement("li");
+    var textnode=document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
